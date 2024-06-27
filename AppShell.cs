b7d31676b3984af1a8764780800e4f723be1188a
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-06-27 12:05:07 +0100

    Wire up network status

--- AppShell.cs
+++ AppShell.cs
@@ -1,15 +1,30 @@
 ﻿
+using System;
 using MauiReactor;
+using MauiReactorPeopleInSpace.Alerts;
+using MauiReactorPeopleInSpace.Extensions;
+using MauiReactorPeopleInSpace.Network;
 using MauiReactorPeopleInSpace.Pages;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Networking;
 
 namespace MauiReactorPeopleInSpace;
 
-class AppShell : Component
+public class AppShellState
+{
+    public NetworkAccess? CurrentNetworkAccess;
+}
+
+partial class  AppShell : Component<AppShellState>
 {  
     private const string MainPageName = "MainPage";
+    private readonly TimeSpan _snackbarDuration = TimeSpan.FromSeconds(3);
     
     private MauiControls.Shell? _shell;
+    
+    [Inject] private readonly INetworkStatusObserver _networkStatusObserver;
+    [Inject] private readonly IUserAlerts _userAlerts;
+    [Inject] private readonly IConnectivity _connectivity;
     
     protected override void OnMounted()
     {
@@ -17,6 +32,39 @@ class AppShell : Component
         Routing.RegisterRoute<DetailPage>(nameof(DetailPage));
 
         base.OnMounted();
+        
+        _networkStatusObserver.Start();
+        
+        _networkStatusObserver.ConnectivityNotifications.Subscribe(networkAccess =>
+        {
+            if (State.CurrentNetworkAccess == networkAccess) return;
+
+            SetState(s => s.CurrentNetworkAccess = networkAccess);
+           
+            _userAlerts.ShowSnackbar(networkAccess != NetworkAccess.Internet
+                    ? "Internet access has been lost."
+                    : "Internet access has been restored.", 
+                _snackbarDuration).FireAndForgetSafeAsync();
+        });
+        
+        CheckInitialNetworkStatus();
+    }
+
+    protected override void OnWillUnmount()
+    {
+        _networkStatusObserver.Dispose();
+        
+        base.OnWillUnmount();
+    }
+
+    private void CheckInitialNetworkStatus()
+    {
+        SetState(s => s.CurrentNetworkAccess = _connectivity.NetworkAccess);
+        if (State.CurrentNetworkAccess != NetworkAccess.Internet)
+        {
+            _userAlerts.ShowSnackbar("No internet access.", _snackbarDuration)
+                .FireAndForgetSafeAsync();
+        }
     }
     
     public override VisualNode Render() =>
